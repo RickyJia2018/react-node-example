b4adb46b69292f2df0f61cbca84c7e0e3133d06c
NingqianJia <ricky.jia2018@gmail.com>
2024-04-12 14:55:38 -0400

    update sql, and front end finished

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,31 +7,64 @@ import { log } from 'winston';
 import { get } from 'lodash';
 const App = () => {
   const API_URL = 'http://localhost:3001' //TODO: move to  .env
-
-  const [pageNumber, setPageNumber] = useState(1)
+  const maxCard = 5;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [hasMore, setHasMore] = useState(false);
+  const [totalCards, setTotalCards] = useState(0);
   const [searchVal, setsearchVal] = useState('');
-  const [cardList, setcardList] = useState<Array<CardModel>>();
-
+  const [cardList, setCardList] = useState<Array<CardModel>>();
+  const [notFoundMsg, setNotFoundMsg] = useState('');
   const debouncedSearch = useDebounce(searchVal, 1000);
-
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(()=>{
-    const fetchData = async() =>{
-      const data = await fetch(
-        `${API_URL}/cards/search?query=${searchVal}&page=${pageNumber}`
-        ).then((res)=>res.json())
-        console.log(data);
-        
-      setcardList(data);
+    const fetchData = () =>{
+
+      try{
+        setIsLoading(true);
+
+        fetch(`${API_URL}/cards/search?query=${searchVal}&page=${pageNumber}`)
+        .then(async (response) => { 
+          const res = await response.json();  
+
+          if (!response.ok) {
+            setIsLoading(false);
+
+            setNotFoundMsg(res.error.details);
+            throw new Error(res.error.details);
+     
+          } 
+          setTotalCards(res.total_cards);
+          setHasMore(res.has_more);
+          processData(res.data);
+          setCardList(res.data);
+          setIsLoading(false);
+
+        })
+      }catch(err){
+        throw err
+      }
 
     }
-    if(debouncedSearch) fetchData();
-  },[debouncedSearch])
+    if(debouncedSearch){
+      setCardList(undefined);
+      setNotFoundMsg('');
+      fetchData();
+    } 
 
+  },[debouncedSearch])
+  const processData = (data: Array<CardModel>) =>{
+
+  }
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setsearchVal(value)
   };
+
+  const handleLoadMoreData: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    console.log("BTN PRESSED");
+};
+
 
   return (
      <div  className="flex flex-col items-center justify-center">
@@ -46,10 +79,30 @@ const App = () => {
           maxLength={1000}
           placeholder='Search for keywords...'
         />
-          
-        <div className='flex flex-wrap justify-center'>
-          {cardList?.map((card, index)=> <Card key={index} data={card}/>)}
-        </div>
+        
+        {
+          isLoading?
+          <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-cyan-500 hover:bg-cyan-400 transition ease-in-out duration-150 cursor-not-allowed" disabled>
+            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns='http://www.w3.org/2000/svg' fill='none' viewBox="0 0 24 24">
+              <circle className='opacity-25' cx="12" cy="12" r="10" stroke='currentColor' strokeWidth={4}></circle>
+              <path className='opacity-75' fill='currentColor' d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+            </svg>
+            Loading...
+          </button>
+          :
+          cardList?
+          <div className='flex flex-wrap justify-center'>
+            {cardList.map((card, index)=> <Card key={index} data={card}/>)}
+            <div className='w-screen'>
+              <button className='relative left-1/2 -translate-x-1/2 m-10 text-slate-100 bg-cyan-600 w-[8rem] h-[3rem] bg-opacity-80  backdrop-blur-[0.5rem] border border-white rounded-full flex items-center justify-center hover:scale-[1.16] active:scale-105 transition-all'
+                onClick={handleLoadMoreData}
+              >Load More</button>
+
+            </div>
+          </div>:
+          <h2>{notFoundMsg}</h2>
+        }
+
     </div>
        
    
--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -11,7 +11,8 @@ function Card({ data }: CardProps) {
     const xRange = [-10, 10];
     const magicCardRef = useRef<HTMLDivElement>(null);
     const propertyRef = useRef<HTMLDivElement>(null);
-
+    const parentRef = useRef<HTMLDivElement>(null);
+    
     function getRotate(range: number[], value: number, max: number) {
         return (value / max) * (range[1] - range[0]) + range[0];
     }
@@ -22,7 +23,6 @@ function Card({ data }: CardProps) {
         const { offsetWidth, offsetHeight } = magicCard as HTMLElement;
         const ry = -getRotate(yRange, offsetX, offsetWidth);
         const rx = getRotate(xRange, offsetY, offsetHeight);
-        console.log(rx,ry);
         
         (magicCard as HTMLElement).style.setProperty('--rx', `${rx}deg`);
         (magicCard as HTMLElement).style.setProperty('--ry', `${ry}deg`);
@@ -30,37 +30,73 @@ function Card({ data }: CardProps) {
     const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) =>{
         const magicCard = magicCardRef.current;
         const propertyCard = propertyRef.current;
-        (propertyCard as HTMLElement).style.display = 'none';
+        const parentCard = parentRef.current;
+        // const nameCard = nameRef.current;
+        // hideElement(nameCard as HTMLElement);
+        hideElement(propertyCard as HTMLElement);
+        hideElement(parentCard as HTMLElement);
 
         (magicCard as HTMLElement).style.setProperty('--rx', `0deg`);
         (magicCard as HTMLElement).style.setProperty('--ry', `0deg`);
     }
     const handleMouseOver = (e: React.MouseEvent<HTMLDivElement>) =>{
         const propertyCard = propertyRef.current;
-        (propertyCard as HTMLElement).style.display = 'block';
+        const parentCard = parentRef.current;
+        showElement(propertyCard as HTMLElement);
+        showElement(parentCard as HTMLElement);
 
+        
+    }
+    const showElement = (e: HTMLElement) =>{
+        e.classList.remove('opacity-0');
+        e.classList.add('opacity-90');
+    }
+    const hideElement = (e: HTMLElement) =>{
+        e.classList.remove('opacity-90');
+        e.classList.add('opacity-0');
     }
     return (
         <>
-        <div className="relative">
-            <div className="magic-card m-5 hover:shadow-lg rounded-xl hover:shadow-cyan-300 " 
-                onMouseMove={handleMouseMove}
-                onMouseLeave={handleMouseLeave}
-                onMouseOver={handleMouseOver}
-                ref={(magicCardRef)}
-            >
-                <img className="w-[20rem] rounded-xl " src={data.image_uris.border_crop} />
-            </div>
-          
-                <div className="absolute  left-1/2 top-0 translate-x-1/2 bg-gray-100 z-10 hidden"
+            <div className="relative">
+                <div className="magic-card m-5 hover:shadow-lg rounded-xl hover:shadow-cyan-300 " 
+                    onMouseMove={handleMouseMove}
+                    onMouseLeave={handleMouseLeave}
+                    onMouseOver={handleMouseOver}
+                    ref={(magicCardRef)}
+                >   
+                {
+                    data.image_uris?
+                    <img className="w-[20rem] rounded-xl " src={data.image_uris.border_crop} />
+                    :
+                    <div className="w-[320px] h-[453px]">NO IMAGE</div>
+                }
+                </div>
+                <div className="text-white p-3 absolute min-w-[200px] left-1/2 bottom-0 -translate-x-1/2 translate-y-full
+                    bg-gray-900 rounded-lg z-10 shadow-lg shadow-slate-50 opacity-0
+                    transition ease-in-out duration-1000"
                 ref={(propertyRef)}>
-                    <h6>Name</h6>
-                    <span>{data.name}</span>
-                    <h6>Set Name</h6>
-                    <span>{data.set_name}</span>
+                    <h6 className="italic underline">Name</h6>
+                    <span  className="text-sm">{data.name}</span>
+                    <h6 className="italic underline">Set Name</h6>
+                    <span className="text-sm">{data.set_name}</span>
+                    <h6 className="italic underline">Collectior Number</h6>
+                    <span  className="text-sm">{data.number}</span>
+                    <h6 className="italic underline">Rarity</h6>
+                    <span className="text-sm">{data.rarity}</span>
                 </div>
 
-      
+                <div className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-full z-10 opacity-0 transition ease-in-out duration-1000"
+                ref={(parentRef)}>
+                    {
+                    data.parent_card&&
+                    <div className={`p-4 w-full h-fit bg-[${data.parent_card.color}] opacity-85 rounded-lg`}>
+                        <h6 className="italic underline">Parent Card</h6>
+                        <span className="text-sm">{data.parent_card.name}</span> 
+                    </div>
+                    }
+                </div>
+
+        
 
             </div>
         </>
